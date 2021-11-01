21bbc3cba6e2b43698c762143a9b8ebf21d03017
Adam Susuł <adamsusul@Adams-MacBook-Pro.local>
2021-11-01 21:02:28 +0100

    feat- add basic implementation of redux toolkit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
+import { Provider } from "react-redux";
 import { Navigation } from "./src/components";
+import { store } from "./src/redux/store";
 
 function App(): JSX.Element {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
 
 export default App;
--- src/screen/AllEmployees.tsx
+++ src/screen/AllEmployees.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 function AllEmployess(): JSX.Element {
+  const screenState = useSelector((state: RootState) => state.userList);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>AllEmployess </Text>
+      {screenState.loading && <Text>LOADIND</Text>}
+      {screenState.error && <Text>Error</Text>}
+      {screenState.users && <Text>user</Text>}
     </View>
   );
 }
