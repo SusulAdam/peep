25867a5bb5770472c027950881202fb9be40d396
Adam Susuł <adamsusul@Adams-MacBook-Pro.local>
2021-11-02 22:59:13 +0100

    feat- finish login screen

--- src/constans/theme.ts
+++ src/constans/theme.ts
@@ -1,9 +1,10 @@
 export const COLORS = {
-  violet: "946CF0",
+  violet: "#946CF0",
   "violet-dark": "#7054AF",
-  white: "#fffff",
+  white: "#ffffff",
   grey_1: "rgba(255,255,255,0.5)",
-  grey_dark_1: "#232229",
-  grey_dark_2: "#111111",
+  grey_dark_1: "rgba(35, 34, 41,0.5)",
+  grey_dark_2: "rgba(7, 17, 17, 0.6)",
   grey_dark_3: "#2B2931",
+  error: "#ff2c2c",
 };
--- src/screen/Login.tsx
+++ src/screen/Login.tsx
@@ -1,7 +1,22 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  Image,
+  ViewStyle,
+  ImageStyle,
+  TextStyle,
+  TextInput,
+  Platform,
+  Pressable,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../constans/theme";
 
 type RootStackParamList = {
   Tabs: undefined;
@@ -10,14 +25,169 @@ type RootStackParamList = {
 type Props = NativeStackScreenProps<RootStackParamList, "Tabs">;
 
 function LoginScreen({ navigation }: Props): JSX.Element {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Login screen </Text>
+  const [userName, onChangeUserName] = React.useState("");
+  const [password, onChangePassword] = React.useState("");
 
-      <Button title="press" onPress={() => navigation.navigate("Tabs")} />
-      <Ionicons name="md-checkmark-circle" size={32} color="green" />
-    </View>
+  const [isCorrectAuth, setIsCorrectAuth] = React.useState(false);
+
+  const handleLogin = () => {
+    if (userName === "Boss" && password === "123") {
+      setIsCorrectAuth(false);
+      navigation.navigate("Tabs");
+    } else {
+      setIsCorrectAuth(true);
+    }
+  };
+
+  return (
+    <ImageBackground
+      source={require("../assets/img/loginBackground.jpg")}
+      style={style.backgroundImage}
+    >
+      <Pressable
+        onPress={() => Keyboard.dismiss()}
+        style={style.dissmissKeyboard}
+      >
+        <View style={style.mainContainer}>
+          <Image
+            source={require("../assets/img/logo.png")}
+            style={style.logo}
+            resizeMode="contain"
+          />
+          <Text style={style.titile}>Welcome </Text>
+          <Text style={style.subTitle}>
+            in employees monitoring app. Being a boss has never been so easy!{" "}
+          </Text>
+          <View style={style.authenticationContainer}>
+            <TextInput
+              value={userName}
+              onChangeText={onChangeUserName}
+              style={style.textInput}
+              placeholder="Username"
+              placeholderTextColor={COLORS.grey_dark_2}
+            />
+            <TextInput
+              value={password}
+              onChangeText={onChangePassword}
+              style={style.textInput}
+              placeholder="Password"
+              secureTextEntry={true}
+              placeholderTextColor={COLORS.grey_dark_2}
+            />
+            {isCorrectAuth && (
+              <Text style={style.errorText}>
+                Incorrect username or password
+              </Text>
+            )}
+            {Platform.OS === "android" ? (
+              <Pressable>
+                <Text style={style.forgotText}>Forgot Password?</Text>
+              </Pressable>
+            ) : (
+              <TouchableOpacity>
+                <Text style={style.forgotText}>Forgot password?</Text>
+              </TouchableOpacity>
+            )}
+            {Platform.OS === "android" ? (
+              <Pressable style={style.button} onPress={handleLogin}>
+                <Text style={style.buttonText}>Login</Text>
+              </Pressable>
+            ) : (
+              <TouchableOpacity
+                style={style.button}
+                activeOpacity={0.8}
+                onPress={handleLogin}
+              >
+                <Text style={style.buttonText}>Login</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+      </Pressable>
+    </ImageBackground>
   );
 }
+
+type Style = {
+  backgroundImage: ViewStyle;
+  logo: ImageStyle;
+  titile: TextStyle;
+  subTitle: TextStyle;
+  authenticationContainer: ViewStyle;
+  textInput: ViewStyle;
+  errorText: TextStyle;
+  button: ViewStyle;
+  mainContainer: ViewStyle;
+  forgotText: TextStyle;
+  buttonText: TextStyle;
+  dissmissKeyboard: TextStyle;
+};
+
+const style: Style = {
+  backgroundImage: {
+    flex: 1,
+  },
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 60,
+  },
+  dissmissKeyboard: {
+    flex: 1,
+  },
+  logo: {
+    maxWidth: 150,
+  },
+  titile: {
+    fontSize: 55,
+    color: COLORS.white,
+  },
+  subTitle: {
+    fontSize: 13,
+    color: COLORS.white,
+    textAlign: "center",
+    width: "60%",
+    paddingTop: 10,
+  },
+  authenticationContainer: {
+    maxHeight: 3000,
+    backgroundColor: COLORS.grey_dark_1,
+    width: "90%",
+    marginVertical: 20,
+    paddingVertical: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    width: "95%",
+    height: 50,
+    marginTop: 15,
+    borderRadius: 30,
+    paddingLeft: 20,
+    backgroundColor: COLORS.white,
+  },
+  forgotText: {
+    color: COLORS.white,
+    marginTop: 10,
+  },
+  button: {
+    width: "95%",
+    backgroundColor: COLORS.violet,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    marginVertical: 15,
+  },
+  buttonText: {
+    color: COLORS.white,
+    fontSize: 20,
+  },
+  errorText: {
+    color: COLORS.error,
+    marginTop: 5,
+  },
+};
 
 export { LoginScreen };
